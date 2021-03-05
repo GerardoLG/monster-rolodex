00aa118205c035c2b4df3e47bfa2e313364f7d11
Gerardo Lopez <59430166+GerardoLG@users.noreply.github.com>
2021-03-04 23:10:59 -0600

    Added handleChange class method into App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
 
 class App extends Component {
   constructor() {
@@ -7,6 +9,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
 
@@ -15,12 +18,25 @@ class App extends Component {
       response.json().then((users) => this.setState({ monsters: users }))
     );
   }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <SearchBox
+          placeholder="Search monster"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
